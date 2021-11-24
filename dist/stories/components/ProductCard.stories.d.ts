025f23d6a3c120539911926b44300297d0c5e6c6
jgimenoperez <jgimenoperez@gmail.com>
2021-11-24 19:24:34 +0100

    feat: Añado declaration.d.ts

--- dist/stories/components/ProductCard.stories.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { MyProductCard } from '../../components/ProductCard';
-declare const _default: ComponentMeta<({ count, maxcount, color, shadow, background, background2, estilofinal }: MyProductCard) => JSX.Element>;
-export default _default;
-export declare const Basic: ComponentStory<({ count, maxcount, color, shadow, background, background2, estilofinal }: MyProductCard) => JSX.Element>;
