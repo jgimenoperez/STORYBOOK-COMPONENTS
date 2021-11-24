36918b3ff845a847d4d33d68310fe37c9d1b4c18
jgimenoperez <jgimenoperez@gmail.com>
2021-11-24 20:05:01 +0100

    feat:Cambios

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,5 @@ export { Button } from './stories/Button';
 export { Header } from './stories/Header';
 export { MyLabel } from './components/MyLabel';
 export { Page } from './stories/Page';
-
+export {MyProductCard} from './components/ProductCard'
+ 
