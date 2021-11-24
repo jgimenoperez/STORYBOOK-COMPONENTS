ba77ced84dd6b948f7c661da4f088962878d7df3
jgimenoperez <jgimenoperez@gmail.com>
2021-11-24 19:36:51 +0100

    feat: Añado declaration.d.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 export { Button } from './stories/Button';
+export { Header } from './stories/Header';
+export { MyLabel } from './components/MyLabel';
+export { Page } from './stories/Page';
 
-// export { Button } from './stories/Button';
-// export { Button } from './stories/Button';
-// export { Button } from './stories/Button';
-// export { Button } from './stories/Button';
